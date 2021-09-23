8181c1a7da8de5d0be30ab638bb79579bb15794a
HoangHuuy <hoanghuy020420@gmail.com>
2021-09-23 23:28:48 +0700

    gio hang

---
+++ resources/views/cart/listCart.blade.php
@@ -0,0 +1,151 @@
+<div class="col-lg-12 col-md-12 col-sm-12 col-12" id="list-cart">
+    {{-- hien thi gio hang              --}}
+    @if(Session::has('cart'))
+    <form>
+        @csrf
+        <div class="table-content table-responsive cart-table-content">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Hình Ảnh</th>
+                        <th>Tên</th>
+                        <th>Đơn Giá</th>
+                        <th>Số Lượng</th>
+                        <th>Số Tiền</th>
+                        <th>Sửa</th>
+                        <th>Xóa</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($products as $product)
+                    <tr>
+                        <td class="product-thumbnail">
+                            <a  href="#">
+                                <img class="img-responsive" src="{{ asset('uploads/product/' .$product['item']['image_product'] )}}" alt="{{ $product['item']['title'] }}" />
+                            </a>
+                        </td>
+                        <td class="product-name">
+                            <a href="#">{{ $product['item']['title'] }}</a>
+                        </td>
+                        <td class="product-price-cart"><span class="amount">
+                            {{ number_format($product['item']['price']) }} &#8363;
+                        </span></td>
+                        <td class="product-quantity">
+                            <div class="cart-plus-minus">
+                                <input id="quanty-item-{{ $product['item']->id }}" class="cart-plus-minus-box" 
+                                type="text" name="qtybutton" value="{{ $product['qty'] }}" />
+                            </div>
+                        </td>
+                        <td class="product-subtotal">
+                            {{ number_format($product['price']) }} &#8363;
+                        </td>
+                        <td class="product-remove">
+                            <a href="#" onclick="SaveItemCart({{ $product['item']['id'] }});"><i class="icon-pencil"></i></a>
+                        </td>
+                        <td class="product-remove">
+                            <a href="#" onclick="DeleteSingleItemCart({{ $product['item']['id'] }});"><i class="icon-close"></i></a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="cart-shiping-update-wrapper">
+                    <div class="cart-shiping-update">
+                        <a href="#">Tiếp tục mua hàng</a>
+                    </div>
+                    <div class="cart-clear">
+                        <button class="update-all">Cập nhập giỏ hàng</button>
+                        <a class="delete-all" href="#">Xóa giỏ hàng</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    <div style="justify-content: right;" class="row">
+        {{-- <div class="col-lg-4 col-md-6 mb-lm-30px">
+            <div class="cart-tax">
+                <div class="title-wrap">
+                    <h4 class="cart-bottom-title section-bg-gray">Estimate Shipping And Tax</h4>
+                </div>
+                <div class="tax-wrapper">
+                    <p>Enter your destination to get a shipping estimate.</p>
+                    <div class="tax-select-wrapper">
+                        <div class="tax-select">
+                            <label>
+                                * Country
+                            </label>
+                            <select class="email s-email s-wid">
+                                <option>Bangladesh</option>
+                                <option>Albania</option>
+                                <option>Åland Islands</option>
+                                <option>Afghanistan</option>
+                                <option>Belgium</option>
+                            </select>
+                        </div>
+                        <div class="tax-select">
+                            <label>
+                                * Region / State
+                            </label>
+                            <select class="email s-email s-wid">
+                                <option>Bangladesh</option>
+                                <option>Albania</option>
+                                <option>Åland Islands</option>
+                                <option>Afghanistan</option>
+                                <option>Belgium</option>
+                            </select>
+                        </div>
+                        <div class="tax-select mb-25px">
+                            <label>
+                                * Zip/Postal Code
+                            </label>
+                            <input type="text" />
+                        </div>
+                        <button class="cart-btn-2" type="submit">Get A Quote</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-lm-30px">
+            <div class="discount-code-wrapper">
+                <div class="title-wrap">
+                    <h4 class="cart-bottom-title section-bg-gray">Use Coupon Code</h4>
+                </div>
+                <div class="discount-code">
+                    <p>Enter your coupon code if you have one.</p>
+                    <form>
+                        <input type="text" required="" name="name" />
+                        <button class="cart-btn-2" type="submit">Apply Coupon</button>
+                    </form>
+                </div>
+            </div>
+        </div> --}}
+        <div class="col-lg-4 col-md-12 mt-md-30px">
+            <div class="grand-totall">
+                <div class="title-wrap">
+                    <h4 class="cart-bottom-title section-bg-gary-cart">Tổng tiền</h4>
+                </div>
+                <h5>Tổng sản phẩm <span>{{ $totalPrice }} &#8363;</span></h5>
+                <div class="total-shipping">
+                    <h5>Total shipping</h5>
+                    <ul>
+                        <li><input type="checkbox" /> Standard <span>$20.00</span></li>
+                        <li><input type="checkbox" /> Express <span>$30.00</span></li>
+                    </ul>
+                </div>
+                <h4 class="grand-totall-title">Tổng <span>{{ $totalPrice }} &#8363;</span></h4>
+                <a href="{{ route('checkout') }}">Tiến hành thanh toán</a>
+            </div>
+        </div>
+    </div>
+    @else
+    <div>
+        <h2>Không có sản phẩm trong giỏ hàng</h2>
+    </div>
+    @endif
+
+    {{-- ket thuc hien thi gio hang --}}
+</div>
+
--- routes/web.php
+++ routes/web.php
@@ -18,29 +18,49 @@ Route::get('/', 'ProductController@indexAll')->name('showAllProduct');
 Auth::routes();
 
 Route::get('/add-to-cart/{id}','CartController@getAddToCart')->name('addToCart');
+
+Route::get('/delete-item-cart/{id}', 'CartController@DeleteItemCart')->name('deleteCart');
+
 Route::get('/shopping-cart','CartController@getCart')->name('shoppingCart');
+
+Route::get('/delete-single-item-cart/{id}','CartController@DeleteSingleItem')->name('deleteSingleItemCart');
+
+Route::get('/save-item-cart/{id}/{qty}','CartController@SaveItemCart')->name('saveItemCart');
+
+Route::get('/check-out','CartController@getCheckOut')->name('checkout')->middleware('auth');
 
 
 Route::group(['prefix' => 'product'], function (){
     Route::get('/{type}', 'ProductController@typeIndex')->name('user.showProduct');
+
     Route::get('/search/id', 'ProductController@searchProduct')->name('user.searchProduct');
 });
 
 Route::group(['prefix' => 'seller'], function (){
     Route::get('/', 'SellerController@index')->name('seller.home');
+
     Route::get('/product/add', 'ProductController@create')->name('seller.addProduct');
+
     Route::post('/product/add', 'ProductController@store')->name('seller.storeProduct');
+
     Route::get('/product/list', 'ProductController@sellerIndex')->name('seller.showProduct');
+
     Route::get('/product/edit/{id}', 'ProductController@edit')->name('seller.editProduct');
+
     Route::post('/product/update/{id}', 'ProductController@update')->name('seller.updateProduct');
+
     Route::get('/product/delete/{id}', 'ProductController@destroy')->name('seller.deleteProduct');
+
     Route::get('/address/show/{id}', 'SellerController@indexAddress')->name('seller.showAddress');
+
     Route::post('/address/add/{id}', 'SellerController@update')->name('seller.updateAddress');
+
     Route::get('/address/add', 'SellerController@index3')->name('seller.addAddress');
 });
 
 Route::group(['prefix' => 'account'], function (){
     Route::get('/profile/{id}', 'AccountController@edit')->name('user.showprofile');
+
     Route::post('/profile/{id}', 'AccountController@store')->name('user.storeAccount');
 });
 
